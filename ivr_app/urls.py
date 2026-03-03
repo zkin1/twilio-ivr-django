cceda4a861cfdb778a2d588e7592ece7b4ab2c47
zkin1 <87878010+zkin1@users.noreply.github.com>
2026-03-03 17:27:49 -0300

    mejora

--- ivr_app/urls.py
+++ ivr_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.voice_prompt, name='voice_prompt'),
     path('menu/', views.voice_menu, name='voice_menu'),
+    path('auto_first/', views.voice_auto_first, name='voice_auto_first'),
     path('status/', views.voice_status, name='voice_status'),
 ]
